4b2042f293dbe96cf26fec8272a3e1f14099f897
Heiner-Pinares <u202212258@upc.edu.pe>
2025-04-20 16:15:32 -0500

    Subiendo backend 2.0

---
+++ src/main/java/pe/edu/upc/trabajo_final/DTOS/UsuarioUDTO.java
@@ -0,0 +1,4 @@
+package pe.edu.upc.trabajo_final.DTOS;
+
+public class UsuarioUDTO {
+}
---
+++ src/main/java/pe/edu/upc/trabajo_final/ServiceInterfaces/IUsuarioService.java
@@ -0,0 +1,11 @@
+package pe.edu.upc.trabajo_final.ServiceInterfaces;
+
+import pe.edu.upc.trabajo_final.Entities.Usuario;
+
+import java.util.List;
+
+public interface IUsuarioService {
+
+    public List<Usuario> list();
+
+}
